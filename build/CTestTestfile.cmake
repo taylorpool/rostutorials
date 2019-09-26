bfbc2e96fea873bb959f217c8527e41418faf08d
Taylor Pool <taylorpool.27@gmail.com>
2019-09-26 15:08:20 -0600

    Added information about creating new packages

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/magicc/rostutorials/src
+# Build directory: /home/magicc/rostutorials/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("beginner_tutorials")
